76ed4f60f8595d3fc04019be60dfc223dfe25ce3
Akshaya Rahate <arahate@ap.corp.jwt.com>
2020-04-16 23:09:15 +0530

    code added in github repo

---
+++ src/main/java/Tp1/Tp1/App.java
@@ -0,0 +1,13 @@
+package Tp1.Tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
